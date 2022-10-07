5e15ea06e6b5770da9bd88b18c7feed82001f0de
Chirag <chiragtilwani08@gmail.com>
2022-10-07 19:11:20 +0530

    add middleware for handling route not found error

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const HttpError= require('./models/http-error')
 const placesRouter= require('./routes/places-routes')
 
 //for parsing encoded data like data of form
@@ -10,6 +10,12 @@ app.use(bodyParser.json())
 
 //for sending request to routes starting with /api/places/... 
 app.use('/api/places',placesRouter)
+
+//handling error for route not found
+app.use((req, res, next) => {
+    const error =new HttpError("could not find this route",404)
+    return next(error)
+})
 
 //error handling middleware
 app.use((error,req,res,next) => {
