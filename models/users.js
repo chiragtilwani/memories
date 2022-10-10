6963aae2b26e59fe1b83b3653276b36ac5f55762
Chirag <chiragtilwani08@gmail.com>
2022-10-10 09:01:43 +0530

    done with user-controllers

--- models/users.js
+++ models/users.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-import { isEmail } from 'validator';
+const { isEmail } =require ('validator');
 
 const userSchema = new mongoose.Schema({
     name:{ type: String, required: true},
