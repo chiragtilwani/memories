97be0bf2605c463d686b667fc685ca2bd9d34077
Chirag <chiragtilwani08@gmail.com>
2022-11-15 00:07:21 +0530

    add port env

--- app.js
+++ app.js
@@ -39,5 +39,5 @@ app.use((error, req, res, next) => {
 })
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() =>
-    app.listen(process.env.PORT)
+    app.listen(process.env.PORT || 5000)
 ).catch((err) => console.log(err))
